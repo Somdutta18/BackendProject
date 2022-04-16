9b8cd31ffd81d99bd93b385ae204d138283d66b7
Prady14 <101656505+Prady14@users.noreply.github.com>
2022-04-16 16:14:53 +0530

    Update recruiterRepo.java

--- src/main/java/com/example/accolite/repository/recruiterRepo.java
+++ src/main/java/com/example/accolite/repository/recruiterRepo.java
@@ -1,5 +1,12 @@
+
 package com.example.accolite.repository;
 
-public interface recruiterRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.accolite.model.recruiter;
+@Repository
+public interface recruiterRepo extends JpaRepository<recruiter, Integer>{
 
 }
+
