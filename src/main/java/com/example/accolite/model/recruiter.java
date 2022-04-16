85059aa8ed1c7e0b399ea09e0596a612025adcb7
Prady14 <101656505+Prady14@users.noreply.github.com>
2022-04-16 16:10:02 +0530

    Update recruiter.java

--- src/main/java/com/example/accolite/model/recruiter.java
+++ src/main/java/com/example/accolite/model/recruiter.java
@@ -1,5 +1,22 @@
+
 package com.example.accolite.model;
 
-public class recruiter {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class recruiter {
+	@Id
+	@GeneratedValue
+	public int id;
+	public String recruiterName;
+	public String email;
 }
